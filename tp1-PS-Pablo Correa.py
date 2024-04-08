f39224a0e029f74c52508890808ec8decfd3a182
Pablo Correa <pejotace22@gmail.com>
2024-04-08 13:09:18 -0300

    Commit inicial

---
+++ tp1-PS-Pablo Correa.py
@@ -0,0 +1,45 @@
+"""Trabajo Práctico Nro 1-Ejercicios en Python"""
+"""1) Escribí un programa que solicite
+ al usuario ingresar un número con decimales y almacenarlo en una variable. A continuación, el programa debe solicitar al usuario que ingrese un número entero y guardarlo en otra variable. En una tercera variable se deberá guardar el resultado de la suma de los dos números ingresados por el usuario. Por último, se debe mostrar en pantalla el texto “El resultado de la suma es [suma]”, donde “[suma]” se reemplazará por el resultado de la operación."""
+
+"""2) Escribí un programa que solicite
+al usuario ingresar la cantidad de kilómetros recorridos por una motocicleta y la cantidad de litros de combustible que consumió durante ese recorrido. Mostrar el consumo de combustible por kilómetro."""
+
+"""3) Escribí un programa que solicite
+ al usuario el ingreso de una temperatura en escala Fahrenheit (debe permitir decimales) y le muestre el equivalente en grados Celsius. La fórmula de conversión que se usa para este cálculo es: _Celsius = (5/9) * (Fahrenheit-32)_"""
+
+"""4) Escribí un programa que solicite
+ al usuario ingresar tres números para luego
+mostrarle el promedio de los tres."""
+
+"""5 )Escribí un programa para solicitar
+ al usuario el ingreso de un número entero y
+que luego imprima un valor de verdad dependiendo de si el número es par o no.
+Recordar que un número es par si el resto, al dividirlo por 2, es 0."""
+
+"""6) Escribí un programa que,
+ dado un número entero, muestre su valor absoluto.
+Recordá que, para los números positivos su valor absoluto es igual al número (el
+valor absoluto de 52 es 52), mientras que, para los negativos, su valor absoluto es el
+número multiplicado por -1 (el valor absoluto de -52 es 52)."""
+
+"""7) Escribí un programa que solicite
+ al usuario el ingreso de dos números diferentes y
+muestre en pantalla al mayor de los dos."""
+"""8)Escribí un programa que,
+ dado un número entero positivo, calcule y muestre su
+factorial. El factorial de un número se obtiene multiplicando todos los números
+enteros positivos que hay entre el 1 y ese número. El factorial de 0 es 1"""
+"""9) Escribí una función llamada
+ esPar que reciba como parámetro un número y
+retorne True si el número es par ó False si es impar. Utilizar esta función en un
+programa que solicite al usuario el ingreso de 10 números y que luego muestre, por
+separado, la suma de todos los pares y la suma de todos los impares."""
+"""10) Escriba una función que tome
+ una lista de números y devuelva la suma
+acumulada, es decir, una nueva lista donde el primer elemento es el mismo, el
+segundo elemento es la suma del primero con el segundo, el tercer elemento es la
+suma del resultado anterior con el siguiente elemento y así sucesivamente. Por
+ejemplo, la suma acumulada de [1,2,3] es [1, 3, 6]."""
+"""11) Escribe una función llamada
+ "elimina" que tome una lista y elimine el primer y último elemento de la lista y cree una nueva lista con los elementos que no fueron eliminados. Luego escribe una función que se llame "media" que tome una lista y devuelva una nueva lista que contenga todos los elementos de la lista anterior menos el primero y el último."""
